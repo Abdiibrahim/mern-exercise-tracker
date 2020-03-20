b90a110e6b7ff50dcd81a5451476c012a3b3e925
Abdi Ibrahim <abdiibrahim850@gmail.com>
2020-03-20 02:49:21 -0400

    Backend functionality

---
+++ backend/routes/users.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+// used to get a user
+// find() method returns a promise
+router.route('/').get((req, res) => {
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// used to add a user
+// save() method saves to db then returns a promise
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
+
+    const newUser = new User({username});
+
+    newUser.save()
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
